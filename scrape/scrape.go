3dd6600e3ed68daaabf7f898985ae0c5298dfa3d
Paul Dunnavant <pdunnavant@gmail.com>
2019-09-03 18:56:08 -0700

    Move ModemInformation to its own source file. (#1)

--- scrape/scrape.go
+++ scrape/scrape.go
@@ -1,7 +1,6 @@
 package scrape
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -11,41 +10,6 @@ import (
 	// "github.com/kr/pretty"
 	"github.com/pdunnavant/modem-scraper/config"
 )
-
-// ModemInformation holds all information from the
-// SB8200 status pages.
-type ModemInformation struct {
-	ConnectionStatus    ConnectionStatus
-	SoftwareInformation SoftwareInformation
-}
-
-// ToJSON converts ModemInformation to JSON string.
-func (m ModemInformation) ToJSON() (string, error) {
-	jsonBytes, err := json.Marshal(m)
-	if err != nil {
-		return "", err
-	}
-	jsonString := string(jsonBytes)
-	return jsonString, nil
-}
-
-// ToLineProtocol converts ModemInformation to "line
-// protocol" for use with InfluxDB.
-// TODO: Really, this should be broken up and implemented in:
-// - downstreambondedchannels ([]string)
-//   - downstreambondedchannel,channel_id=# channel_id=#,frequency=#,etc
-// - upstreambondedchannels ([]string)
-// - softwareinformation
-// - startupprocedure
-//
-// or maybe... do the above, and also have this (ModemInformation)
-// call each of the above to aggregate them all. Then this can
-// simply return []string with all the "lines" and they can
-// all be sent to InfluxDB at once.
-func (m ModemInformation) ToLineProtocol() ([]string, error) {
-	// TODO
-	return []string{}, nil
-}
 
 // Scrape scrapes data from the modem.
 func Scrape(config config.Configuration) (*ModemInformation, error) {
