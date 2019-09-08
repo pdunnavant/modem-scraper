ffcea1669340b5bca9d1f7e2001b22d3d0f06cb0
Paul Dunnavant <pdunnavant@gmail.com>
2019-09-07 22:28:21 -0700

    Prep go binary releases.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,11 +4,14 @@ BUILD_VERSION="$(date +'%Y%m%d%H%M%S')-$(git log --format=%h -1)"
 
 echo "Building binary with version [${BUILD_VERSION}]..."
 
+BINARY_NAME="modem-scraper"
 FLAGS="-X main.BuildVersion=${BUILD_VERSION}"
-go build -ldflags="${FLAGS}"
-
+go build -ldflags="${FLAGS}" -o ${BINARY_NAME}
 echo "Built version:"
 ./modem-scraper -version
+
+ARCHIVE_NAME="${BINARY_NAME}_${BUILD_VERSION}_linux_amd64.zip"
+zip ${ARCHIVE_NAME} ${BINARY_NAME}
 
 if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST_BRANCH}" = "" ] ; then
   echo "Detected master branch; pushing version tag to repository..."
